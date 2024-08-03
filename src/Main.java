2f0c538ecffec7f677cf8ea4c9cfcb4854299709
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-08-03 06:01:57 +0000

    Stack constructor and push method added

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        
+        Stack myStack = new Stack(4);
+
+        // myStack.getTop();
+        // myStack.getHeight();
+
+        myStack.push(5);
+
+        myStack.printStack();
+
+    }
+}
